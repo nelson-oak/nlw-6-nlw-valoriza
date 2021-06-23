190839107e143036cdfd89ae03a70281bb95dfa3
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-06-22 21:20:48 -0300

    Aula 3. Cadastro de tags e middleware de admin

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express'
+import { CreateTagController } from './controllers/CreateTagController'
 import { CreateUserController } from './controllers/CreateUserController'
+import { ensureAdmin } from './middlewares/ensureAdmin'
 
 const createUserController = new CreateUserController()
+const createTagController = new CreateTagController()
 
 const router = Router()
 
 router.post('/users', createUserController.handle)
+router.post('/tags', ensureAdmin, createTagController.handle)
 
 export { router }
